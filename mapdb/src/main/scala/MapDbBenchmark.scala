d251be47cb29f0eb5d3624d1b5ce42d32a5a8396
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-12 16:40:28 +1200

    mapdb: build multiple maps (combine at the end)

--- mapdb/src/main/scala/MapDbBenchmark.scala
+++ mapdb/src/main/scala/MapDbBenchmark.scala
@@ -34,51 +34,72 @@ object MapDbBenchmark extends App {
 
   val start = System.currentTimeMillis
 
-  val iter: java.util.Iterator[Pair[java.lang.Long, Array[Byte]]] =
-    0.until(FileCount).map(fileName).flatMap { fileName =>
-        val fileIn = new FileInputStream(fileName)
-        var valueCount = 0
-        val tmpArray = new Array[Byte](ValueByteCount)
-        val builder = ArrayBuffer.newBuilder[Pair[java.lang.Long, Array[Byte]]]
-        while (valueCount < ValueCountPerFile) {
-          val bytesRead = fileIn.read(tmpArray)
-          assert(bytesRead == ValueByteCount, s"expected $ValueByteCount bytes to be read, but only got $bytesRead")
-          builder.addOne(new Pair(currId.getAndIncrement, tmpArray))
-          valueCount += 1
-        }
-        fileIn.close
-        builder.result
-    }.iterator.asJava
-
-  val nodesTreeMap: BTreeMap[java.lang.Long, Array[Byte]] = db.treeMap("nodes")
-    .keySerializer(Serializer.LONG)
-    .valueSerializer(Serializer.BYTE_ARRAY)
-//    .createOrOpen()
-      .createFrom(iter)
-
-////  val futures = 0.until(FileCount).map(fileName).map { fileName =>
-//  val futures = 0.until(2).map(fileName).map { fileName =>
-//    executorService.submit(new Runnable {
-//      override def run(): Unit = {
+//  val iter: java.util.Iterator[Pair[java.lang.Long, Array[Byte]]] =
+//    0.until(FileCount).map(fileName).flatMap { fileName =>
 //        val fileIn = new FileInputStream(fileName)
 //        var valueCount = 0
 //        val tmpArray = new Array[Byte](ValueByteCount)
+//        val builder = ArrayBuffer.newBuilder[Pair[java.lang.Long, Array[Byte]]]
 //        while (valueCount < ValueCountPerFile) {
 //          val bytesRead = fileIn.read(tmpArray)
 //          assert(bytesRead == ValueByteCount, s"expected $ValueByteCount bytes to be read, but only got $bytesRead")
-//          nodesTreeMap.put(currId.getAndIncrement, tmpArray)
+//          builder.addOne(new Pair(currId.getAndIncrement, tmpArray))
 //          valueCount += 1
 //        }
-//        // TODO only do at the very end?
-////        db.commit
 //        fileIn.close
-//      }
-//    })
-//  }
-//
-//  println("awaiting all futures...")
-//  futures.foreach(_.get)
-//  println("finished all futures - closing storage now")
+//        builder.result
+//    }.iterator.asJava
+
+//  val nodesTreeMap: BTreeMap[java.lang.Long, Array[Byte]] = db.treeMap("nodes")
+//    .keySerializer(Serializer.LONG)
+//    .valueSerializer(Serializer.BYTE_ARRAY)
+//    .createFrom(iter)
+
+  type TreeMap = BTreeMap[java.lang.Long, Array[Byte]]
+
+  val trees = ArrayBuffer.newBuilder[TreeMap]
+
+  val futures = 0.until(FileCount).map(fileName).map { fileName =>
+//  val futures = 0.until(2).map(fileName).map { fileName =>
+    executorService.submit(new Runnable {
+      override def run() = {
+        val iter: java.util.Iterator[Pair[java.lang.Long, Array[Byte]]] = {
+          val fileIn = new FileInputStream(fileName)
+          var valueCount = 0
+          val tmpArray = new Array[Byte](ValueByteCount)
+          val builder = ArrayBuffer.newBuilder[Pair[java.lang.Long, Array[Byte]]]
+          while (valueCount < ValueCountPerFile) {
+            val bytesRead = fileIn.read(tmpArray)
+            assert(bytesRead == ValueByteCount, s"expected $ValueByteCount bytes to be read, but only got $bytesRead")
+            builder.addOne(new Pair(currId.getAndIncrement, tmpArray))
+            valueCount += 1
+          }
+          fileIn.close
+          builder.result
+        }.iterator.asJava
+
+        val nodesTreeMap: TreeMap = db.treeMap(fileName)
+          .keySerializer(Serializer.LONG)
+          .valueSerializer(Serializer.BYTE_ARRAY)
+          .createFrom(iter)
+        // TODO only do at the very end?
+//        db.commit
+//        fileIn.close
+        trees.addOne(nodesTreeMap)
+      }
+    })
+  }
+
+  println("awaiting all futures...")
+  futures.foreach(_.get)
+  println("finished all futures - closing storage now")
+
+//  val b: TreeMap = ???
+//  b.putAll(b)
+
+//  trees.result.toList.reduce(_.merge())
+
+  // TODO combine all trees
 
   db.commit
   db.close
