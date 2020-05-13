1d436f63289e8ae3aefdc9011001d5f812115da3
Michael Pollmeier <michael@michaelpollmeier.com>
2020-05-13 21:42:15 +1200

    add Plainfilebenchmarkwithrandom

--- common/src/main/scala/PlainFileBenchmarkWithRandom.scala
+++ common/src/main/scala/PlainFileBenchmarkWithRandom.scala
@@ -1,12 +1,14 @@
-import java.io.{File, FileInputStream, FileOutputStream}
+import java.io.{File, FileOutputStream}
 import java.nio.file.{Files, Paths}
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
 import Setup._
 
+import scala.util.Random
+
 /* write a plain file to see the theoretical maximum */
-object PlainFileBenchmark extends App {
+object PlainFileBenchmarkWithRandom extends App {
   val threadCount = args match {
     case Array(threadCount) => threadCount.toInt
     case _ => throw new AssertionError("expected exactly one argument: threadCount")
@@ -15,26 +17,24 @@ object PlainFileBenchmark extends App {
   val currId = new AtomicInteger(0)
   val executorService = Executors.newFixedThreadPool(threadCount)
   val storageFile = Files.createTempDirectory(Paths.get("target"), getClass.getName)
+  val rnd = new Random
 
   val start = System.currentTimeMillis
   val futures = 0.until(FileCount).map { inputFileIdx =>
     executorService.submit(new Runnable {
       override def run(): Unit = {
-        val fileIn = new FileInputStream(fileName(inputFileIdx))
         var valueCount = 0
         val tmpArray = new Array[Byte](ValueByteCount)
         val byteUtils = new ByteUtils
         // TODO write to one file rather than $threadCount
         val fileOutStream = new FileOutputStream(new File(s"$storageFile/$inputFileIdx"))
         while (valueCount < ValueCountPerFile) {
-          val bytesRead = fileIn.read(tmpArray)
-          assert(bytesRead == ValueByteCount, s"expected $ValueByteCount bytes to be read, but only got $bytesRead")
+          rnd.nextBytes(tmpArray)
           val idBytes = byteUtils.longToBytes(currId.getAndIncrement)
           fileOutStream.write(idBytes)
           fileOutStream.write(tmpArray)
           valueCount += 1
         }
-        fileIn.close
       }
     })
   }
